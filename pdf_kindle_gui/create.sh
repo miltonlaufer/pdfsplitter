d0b901f7af3f4ec67d70bf4386d2f9341fde89b7
oskosk <oskosk@gmail.com>
2018-02-03 10:52:20 -0300

    Make create.sh be a shell script

--- pdf_kindle_gui/create.sh
+++ pdf_kindle_gui/create.sh
@@ -1,2 +1,13 @@
-#pyinstaller --onefile --windowed --add-data 'icon.png:.' --icon=icon.ico --clean pdf_kindle_gui.py 
+#!/bin/sh
+
+workpath=./build
+distpath=./dist
+specpath=./
+if [ "$1" == "macos" ]; then
+	workpath="./macos/build"
+	distpath="./macos/dist"
+	specpath="./macos/"
+fi
+
+pyinstaller --onefile --windowed --add-data 'icon.png:.' --icon=icon.ico --clean pdf_kindle_gui.py --workpath "$workpath" --distpath "$distpath" --specpath "$specpath"
 
